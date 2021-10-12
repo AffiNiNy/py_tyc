cfeaf80ef1ea3373f58b950f0a8912f33f15fa73
hujs <hujs@augurit.com>
2021-10-12 17:30:53 +0800

    1

--- demo/xlwings/index.py
+++ demo/xlwings/index.py
@@ -2,21 +2,30 @@ import xlwings as xw
 import os
 import sys
 sys.path.append(os.getcwd())
-from myio.xlwings.index import SelfXlwings
+from myio.xlwings.SelfXlwings import *
 
-selfXW = SelfXlwings(SelfXlwings.initApp())
-wb = selfXW.new_xlsx()
+selfxw = SelfXlwings()
+wb = selfxw.new_xlsx()
+comIntroSht = wb.sheets[0]
+comIntroSht.range('A1').value = 1243
+comIntroSht.name = '公司简介'
+print(comIntroSht.name)
+print(comIntroSht.range('A1').value)
+wb.save(r'D:\DEVELOP\\VSCode_Projects\ALittlePythonProg\demo\\output_Excel.xlsx')
+# wb.save()
 
-sheet = wb.sheets[0]
-# sheet.range('A1').value = 'Foo 1'
-#python中的一维列表，在Excel中默认为一行数据
-# rng1 = sheet.range('A1').value = [[1,2], [3,4]]
-#纵向插入数据
-rng1 = sheet.range('A1').options(transpose=True).value = [[1,2,22], [3,4,44]]
-print(rng1)
+selfxw.quit()
 
-wb.save(r'D:\DEVELOP\\VSCode_Projects\ALittlePythonProg\demo\\new_Excel.xlsx')
-wb.close()
+# sheet = wb.sheets[0]
+# # sheet.range('A1').value = 'Foo 1'
+# #python中的一维列表，在Excel中默认为一行数据
+# # rng1 = sheet.range('A1').value = [[1,2], [3,4]]
+# #纵向插入数据
+# rng1 = sheet.range('A1').options(transpose=True).value = [[1,2,22], [3,4,44]]
+# print(rng1)
 
-#结束进程
-selfXW.quit()
+# wb.save(r'D:\DEVELOP\\VSCode_Projects\ALittlePythonProg\demo\\new_Excel.xlsx')
+# wb.close()
+
+# #结束进程
+# selfXW.quit()
--- myio/xlwings/SelfXlwings.py
+++ myio/xlwings/SelfXlwings.py
@@ -1,29 +1,26 @@
 import xlwings as xw
-from xlwings.main import App, Books
+from xlwings.main import App
 
 class SelfXlwings:
     'xlwings 方法'
 
-    def __init__(self, app) -> None:
-        self.app = app
+    def __init__(self):
+        self.app = SelfXlwings.initApp()
 
     @staticmethod
     def initApp():
-        app = xw.App(visible=False,add_book=False)
+        app = xw.App(visible=False,add_book=True)
         #不显示Excel消息框
         app.display_alerts = False
         #关闭屏幕更新,可加快宏的执行速度
         app.screen_updating = False
         return app
     
-    @classmethod
-    def new_xlsx(self) -> Books:
+    def new_xlsx(self):
         return self.app.books.add()
     
-    @classmethod
-    def print_attr(self):
-        print(self.num)
+    def open_xlsx(self, xlsxPath):
+        return self.app.books.open(xlsxPath)
     
-    @classmethod
     def quit(self):
         self.app.quit()
